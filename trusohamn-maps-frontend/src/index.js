73f3afa93cf69468582e7a57326b0dc18f7227aa
trusohamn <marta.trusohamn@gmail.com>
2019-06-24 16:39:25 +0200

    map-setup

--- trusohamn-maps-frontend/src/index.js
+++ trusohamn-maps-frontend/src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+import { fromLonLat, toLonLat } from 'ol/proj';
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+
+
+const map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        })
+    ],
+
+    view: new View({
+        center: fromLonLat([17.762083241832443, 59.401848231069636]),
+        zoom: 17
+    })
+});
+
+
+const source = new VectorSource();
+const features = [];
+const coords = fromLonLat([17.762083241832443, 59.401848231069636]);
+features.push(new Feature({
+    geometry: new Point(coords)
+}));
+source.addFeatures(features);
+
+const v = new VectorLayer({
+    source: source
+})
+map.addLayer(v);
+
+
+map.on('click', function (event) {
+    console.log(toLonLat(event.coordinate));
+
+});
+
+
+
