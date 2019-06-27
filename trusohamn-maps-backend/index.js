b8f06acf48ae16a4e541a5bdbf427c13235da0c7
trusohamn <marta.trusohamn@gmail.com>
2019-06-27 08:34:21 +0200

    checkin

---
+++ trusohamn-maps-backend/index.js
@@ -0,0 +1,48 @@
+const port = process.env.PORT || 8000;
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.urlencoded());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.post('/api/points', (req, res) => {
+    const points = require('./points.json');
+    const point = {
+        localisation: [parseFloat(req.body.lon), parseFloat(req.body.lat)],
+        description: req.body.description
+    }
+    console.log(point);
+    points.points.push(point);
+    fs.writeFile('./points.json', JSON.stringify(points), () => {
+        res.status(200).end();
+    });
+});
+
+app.get('/api/points', (req, res) => {
+    const points = require('./points.json');
+    res.send(JSON.stringify(points));
+});
+
+app.use((req, res, next) => {
+    const err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res) {
+    console.error(err.stack);
+    res.status(500).send('Something broke! ' + err.message);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
---
+++ trusohamn-maps-frontend/src/components/Map.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import Form from './Form';
+import logo from '../icons/039-tent.svg';
+
+
+const ol = require('openlayers');
+require('openlayers/css/ol.css');
+
+class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = ({
+            lon: 18,
+            lat: 60,
+            data: null
+        });
+    }
+
+    componentDidMount() {
+        const extraLayer = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                features: []
+            })
+        });
+        const featuresLayer = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                features: []
+            })
+        });
+
+        var map = new ol.Map({
+            target: this.refs.mapContainer,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                }),
+                extraLayer,
+                featuresLayer
+            ], view: new ol.View({
+                center: ol.proj.fromLonLat([17.762083241832443, 59.401848231069636]),
+                zoom: 13,
+            })
+        });
+
+        map.on('click', this.handleMapClick.bind(this));
+
+        this.setState({
+            map: map,
+            extraLayer: extraLayer,
+            featuresLayer: featuresLayer
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.state.data === null){
+            console.log('updating data');
+            fetch('http://localhost:8000/api/points')
+                .then(res => res.json())
+                .then(data => {
+                    this.setState({
+                        data: data
+                    });
+    
+                    const features = [];
+    
+                    const icon = new ol.style.Icon({
+                        opacity: 1,
+                        scale: 0.03,
+                        src: logo
+                    });
+    
+                    const iconStyle = new ol.style.Style({
+                        image: icon
+                    });
+    
+                    data.points.forEach((e) => {
+                        const coords = ol.proj.fromLonLat(e.localisation);
+                        const iconFeature = new ol.Feature({
+                            geometry: new ol.geom.Point(coords),
+                            name: e.description
+                        });
+                        iconFeature.setStyle(iconStyle);
+                        features.push(iconFeature);
+                    })
+    
+                    this.state.featuresLayer.setSource(
+                        new ol.source.Vector({
+                            features: features
+                        })
+                    );
+                })
+                .catch(error => console.log(error));
+        }
+    }
+
+    handleMapClick(event) {
+
+        this.state.map.forEachFeatureAtPixel(event.pixel,
+            feature => {
+                console.log(feature.get('name'));
+            });
+        ////////////////
+        const coord = ol.proj.toLonLat(event.coordinate);
+        console.log(coord);
+        this.setState({
+            lon: coord[0],
+            lat: coord[1]
+        });
+
+        const features = [];
+
+        const coords = event.coordinate;
+        const newFeature = new ol.Feature({
+            geometry: new ol.geom.Point(coords)
+        })
+
+        features.push(newFeature);
+
+        this.state.extraLayer.setSource(
+            new ol.source.Vector({
+                features: features
+            })
+        );
+    }
+
+    render() {
+        return (
+            <div ref="mapContainer">
+                <Form lon={this.state.lon} lat={this.state.lat} removeData={() => this.setState({ data: null })}></Form>
+            </div>
+        );
+    }
+
+}
+export default Map;
