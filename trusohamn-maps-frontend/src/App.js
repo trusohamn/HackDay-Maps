96a1b6169ffbd89c7cfe6d59d182d417cfa74a65
trusohamn <marta.trusohamn@gmail.com>
2019-06-27 13:37:01 +0200

    hiding and showing description and switching reviews

--- trusohamn-maps-frontend/src/App.js
+++ trusohamn-maps-frontend/src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Map from './components/Map';
 import Description from './components/Description';
 function App() {
-  const [pointDescription, setPointDescription] = useState({})
+  const [pointDescription, setPointDescription] = useState({});
 
   return (
     <div className="App">
