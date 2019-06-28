39a3b04ec77b0486e1fe7ebf3d98f526fb551698
trusohamn <marta.trusohamn@gmail.com>
2019-06-28 13:37:05 +0200

    Updates

---
+++ precache-manifest.443af36f7e8dad536ac015db6737fb98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1402ea1380529ab570bcde9c23431dae",
+    "url": "/HackDay-Maps/index.html"
+  },
+  {
+    "revision": "edf60e2ff4b8c50345ab",
+    "url": "/HackDay-Maps/static/css/2.689a54e8.chunk.css"
+  },
+  {
+    "revision": "a77730f65aef5c05bcaa",
+    "url": "/HackDay-Maps/static/css/main.1cfde8c9.chunk.css"
+  },
+  {
+    "revision": "edf60e2ff4b8c50345ab",
+    "url": "/HackDay-Maps/static/js/2.eb952bf3.chunk.js"
+  },
+  {
+    "revision": "a77730f65aef5c05bcaa",
+    "url": "/HackDay-Maps/static/js/main.9012f5f1.chunk.js"
+  },
+  {
+    "revision": "25f26b99ea116aa3b299",
+    "url": "/HackDay-Maps/static/js/runtime~main.1c9e6921.js"
+  },
+  {
+    "revision": "6b6a714cfdaa5ef3c1d6a7a89fce348f",
+    "url": "/HackDay-Maps/static/media/009-binoculars.6b6a714c.svg"
+  },
+  {
+    "revision": "21b76057d6a38e0ba0765a897148aa30",
+    "url": "/HackDay-Maps/static/media/010-bonfire.21b76057.svg"
+  },
+  {
+    "revision": "ebd03a546f91a0000481027ab8794350",
+    "url": "/HackDay-Maps/static/media/039-tent.ebd03a54.svg"
+  }
+]);
