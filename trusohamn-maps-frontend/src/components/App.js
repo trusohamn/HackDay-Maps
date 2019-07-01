03bbf4024864099f27e7eb5053dd2e49fc8576db
trusohamn <marta.trusohamn@gmail.com>
2019-07-01 12:13:39 +0200

    generating Redirect to point path

--- trusohamn-maps-frontend/src/components/App.js
+++ trusohamn-maps-frontend/src/components/App.js
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import Location from './Location';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import { MyContext } from '../contexts/MyContextProvider';
 
 
 function App() {
   const context = useContext(MyContext);
   const switchText = (context.mode === 'explore') ? 'Edit map' : 'Explore';
+  const [redirect, setRedirect] = useState(null)
+  useEffect(() => {
+    console.log('fromm app, point id change to', context.pointId)
+    const newPath = "/" + context.pointId
+    setRedirect(<Redirect to={newPath}></Redirect>)
+  }, [context.pointId])
   return (
     <Router>
       <div className="App">
@@ -17,6 +23,7 @@ function App() {
           </h1>
           <button id="switch modes" onClick={context.switchMode}>{switchText}</button>
         </nav>
+        {redirect}
         <Route exact path="/" component={Location} />
         <Route path="/:id" component={Location} />
       </div>
