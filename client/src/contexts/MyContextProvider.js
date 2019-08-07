353ba8a1b55e5519dc228cb13cef0de024466427
trusohamn <marta.trusohamn@gmail.com>
2019-08-07 12:33:10 -0700

    style changes

--- client/src/contexts/MyContextProvider.js
+++ client/src/contexts/MyContextProvider.js
@@ -1,49 +1,51 @@
-import React, { useState } from 'react'
-export const MyContext = React.createContext({})
+import React, { useState } from "react";
+export const MyContext = React.createContext({});
 
 function MyContextProvider(props) {
-    const [pointId, setPointId] = useState(null);
-    const [mode, setMode] = useState('explore');
-    const [data, setData] = useState(null);
-    const [lon, setLon] = useState(17.86208324183244);
-    const [lat, setLat] = useState(59.30184823106963);
-    const [cyclingOn, setCyclingOn] = useState(false);
-    const [hikingOn, setHikingOn] = useState(false);
-    const [ hikebikeOn, setHikebikeOn] = useState(false)
-    const [hillshadingOn, setHillshadingOn] = useState(false);
+  const [pointId, setPointId] = useState(null);
+  const [mode, setMode] = useState("explore");
+  const [data, setData] = useState(null);
+  const [lon, setLon] = useState(17.86208324183244);
+  const [lat, setLat] = useState(59.30184823106963);
+  const [cyclingOn, setCyclingOn] = useState(false);
+  const [hikingOn, setHikingOn] = useState(false);
+  const [hikebikeOn, setHikebikeOn] = useState(false);
+  const [hillshadingOn, setHillshadingOn] = useState(false);
 
-    const switchMode = () => {
-        const newMode = (mode === 'explore') ? 'edit' : 'explore';
-        setMode(newMode);
-    };
-    
-    const getPointIdData = (id=pointId) => {
-        const pointData =  data.find( d => d._id == id);
-        return pointData;
-    }
+  const switchMode = () => {
+    const newMode = mode === "explore" ? "edit" : "explore";
+    setMode(newMode);
+  };
 
+  const getPointIdData = (id = pointId) => {
+    const pointData = data.find(d => d._id == id);
+    return pointData;
+  };
 
-    const state = {
-        pointId,
-        setPointId,
-        getPointIdData,
-        mode,
-        setMode,
-        switchMode,
-        data,
-        setData,
-        lon, setLon,
-        lat, setLat,
-        cyclingOn, setCyclingOn,
-        hikingOn, setHikingOn,
-        hikebikeOn, setHikebikeOn,
-        hillshadingOn, setHillshadingOn
-    }
-    return (
-        <MyContext.Provider value={state}>
-            {props.children}
-        </MyContext.Provider>
-    )
-
+  const state = {
+    pointId,
+    setPointId,
+    getPointIdData,
+    mode,
+    setMode,
+    switchMode,
+    data,
+    setData,
+    lon,
+    setLon,
+    lat,
+    setLat,
+    cyclingOn,
+    setCyclingOn,
+    hikingOn,
+    setHikingOn,
+    hikebikeOn,
+    setHikebikeOn,
+    hillshadingOn,
+    setHillshadingOn
+  };
+  return (
+    <MyContext.Provider value={state}>{props.children}</MyContext.Provider>
+  );
 }
-export default MyContextProvider
+export default MyContextProvider;
--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,72 +1,83 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import Location from './Location';
-import { Route } from 'react-router-dom';
-import { MyContext } from '../contexts/MyContextProvider';
-import { AuthContext } from '../contexts/AuthContextProvider';
-
-
+import Location from "./Location";
+import { Route } from "react-router-dom";
+import { MyContext } from "../contexts/MyContextProvider";
+import { AuthContext } from "../contexts/AuthContextProvider";
 
 function App() {
   const context = useContext(MyContext);
   const authContext = useContext(AuthContext);
 
-  const switchText = (context.mode === 'explore') ? 'Edit map' : 'Explore';
+  const switchText = context.mode === "explore" ? "Edit map" : "Explore";
 
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     const target = event.target;
     const value = target.checked;
     const name = target.name;
 
-    switch(name){
-      case 'cyclingOn' : return context.setCyclingOn(value);
-      case 'hikingOn' : return context.setHikingOn(value);
-      case 'hikebikeOn' : return context.setHikebikeOn(value);
-      case 'hillshadingOn' : return context.setHillshadingOn(value);
-      default : return
+    switch (name) {
+      case "cyclingOn":
+        return context.setCyclingOn(value);
+      case "hikingOn":
+        return context.setHikingOn(value);
+      case "hikebikeOn":
+        return context.setHikebikeOn(value);
+      case "hillshadingOn":
+        return context.setHillshadingOn(value);
+      default:
+        return;
     }
-  }
+  };
 
   return (
-
     <div className="App">
-      { authContext.isAuthenticated ? 
-      <button id="switch modes" onClick={context.switchMode}>{switchText}</button> :
-      null
-    }
+      {authContext.isAuthenticated ? (
+        <button id="switch modes" onClick={context.switchMode}>
+          {switchText}
+        </button>
+      ) : null}
 
-    <form id="layers">
+      <form id="layers">
         <label>
-        <a href="https://cycling.waymarkedtrails.org/">waymarkedtrails cycling  </a>
+          <a href="https://cycling.waymarkedtrails.org/">
+            waymarkedtrails cycling{" "}
+          </a>
           <input
             name="cyclingOn"
             type="checkbox"
             checked={context.cyclingOn}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          />
         </label>
         <label>
-        <a href="https://hiking.waymarkedtrails.org/">waymarkedtrails hiking  </a>
+          <a href="https://hiking.waymarkedtrails.org/">
+            waymarkedtrails hiking{" "}
+          </a>
           <input
             name="hikingOn"
             type="checkbox"
             value={context.hikingOn}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          />
         </label>
         <label>
-        <a href="http://hikebikemap.org">wmflabs Hike Bike  </a>
+          <a href="http://hikebikemap.org">wmflabs Hike Bike </a>
           <input
             name="hikebikeOn"
             type="checkbox"
             value={context.hikebikeOn}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          />
         </label>
         <label>
-        <a href="http://hikebikemap.org">wmflabs Hillshading  </a>
+          <a href="http://hikebikemap.org">wmflabs Hillshading </a>
           <input
             name="hillshadingOn"
             type="checkbox"
             value={context.hillshadingOn}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          />
         </label>
       </form>
       {context.redirect}
