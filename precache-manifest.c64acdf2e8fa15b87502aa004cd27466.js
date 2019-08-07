57ed8796f8ace5321a44ece226d92cf6d53aa1da
trusohamn <marta.trusohamn@gmail.com>
2019-08-07 12:37:06 -0700

    Updates

---
+++ precache-manifest.c64acdf2e8fa15b87502aa004cd27466.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f049a2a4ace1a2e3ffa529adec19a6f4",
+    "url": "/Tently/index.html"
+  },
+  {
+    "revision": "f7fc38a87f2da09c0bf6",
+    "url": "/Tently/static/css/2.03649bd7.chunk.css"
+  },
+  {
+    "revision": "02a180a7921b2c1da794",
+    "url": "/Tently/static/css/main.24e518f0.chunk.css"
+  },
+  {
+    "revision": "f7fc38a87f2da09c0bf6",
+    "url": "/Tently/static/js/2.f735f485.chunk.js"
+  },
+  {
+    "revision": "02a180a7921b2c1da794",
+    "url": "/Tently/static/js/main.60dc983f.chunk.js"
+  },
+  {
+    "revision": "0122d286d8b5d07e5991",
+    "url": "/Tently/static/js/runtime~main.ee27bf39.js"
+  },
+  {
+    "revision": "6b6a714cfdaa5ef3c1d6a7a89fce348f",
+    "url": "/Tently/static/media/009-binoculars.6b6a714c.svg"
+  },
+  {
+    "revision": "21b76057d6a38e0ba0765a897148aa30",
+    "url": "/Tently/static/media/010-bonfire.21b76057.svg"
+  },
+  {
+    "revision": "8f0eb9f0a0ebbcc921a301e6e9a8b0bf",
+    "url": "/Tently/static/media/012-camp.8f0eb9f0.svg"
+  },
+  {
+    "revision": "f466d3ecf29e80bb22149bb80021a63f",
+    "url": "/Tently/static/media/032-hut.f466d3ec.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HackDay-Maps/precache-manifest.613c07df91b97f31190dee5f343991b7.js"
+  "/Tently/precache-manifest.c64acdf2e8fa15b87502aa004cd27466.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HackDay-Maps/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tently/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
